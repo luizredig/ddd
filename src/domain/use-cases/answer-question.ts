39f8b7ef6ead92fcbf2698e9502318e4ccfebf43
redig <luizredig@gmail.com>
2025-03-03 13:02:17 -0300

    refactor(use-cases): use new static method create in answer question use case

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,6 @@
+import { UniqueEntityID } from "src/core/entities/unique-entity-id";
 import Answer from "../entities/answer";
+import { AnswersRepository } from "../repositories/answers-repository";
 
 interface AnswerQuestionUseCaseRequest {
   questionId: string;
@@ -7,12 +9,20 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-  handle({
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async handle({
     questionId,
     instructorId,
     content,
-  }: AnswerQuestionUseCaseRequest): Answer {
-    const answer = new Answer({ content, authorId: instructorId, questionId });
+  }: AnswerQuestionUseCaseRequest): Promise<Answer> {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    });
+
+    await this.answersRepository.createAnswer(answer);
 
     return answer;
   }
