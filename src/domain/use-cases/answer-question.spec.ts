190a717ac8aee8be72aa794c6f8fe06835cfb3d8
redig <luizredig@gmail.com>
2025-03-03 13:03:51 -0300

    refactor(spec): use answers repository in create an answer test

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,16 +1,22 @@
 import Answer from "../entities/answer";
+import { AnswersRepository } from "../repositories/answers-repository";
 import { AnswerQuestionUseCase } from "./answer-question";
 import { expect, test } from "vitest";
 
-test("Create an answer question use case", () => {
-  const answerQuestion = new AnswerQuestionUseCase();
+const fakeAnswersRepository: AnswersRepository = {
+  createAnswer: async (answer: Answer) => {
+    return answer;
+  },
+};
 
-  const answer = answerQuestion.handle({
+test("Create an answer question use case", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+
+  const answer = await answerQuestion.handle({
     questionId: "questionId",
     instructorId: "instructorId",
     content: "content",
   });
 
-  expect(answer).toBeInstanceOf(Answer);
-  expect(answer.content).toBe("content");
+  expect(answer.content).toEqual("content");
 });
