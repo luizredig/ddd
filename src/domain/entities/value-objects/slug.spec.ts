47cfe6cf57625444e74517c998ef1cf9192107ea
redig <luizredig@gmail.com>
2025-03-03 12:59:23 -0300

    feat(value-objects): add Slug

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test("Create a slug from a string", () => {
+  const slug = Slug.createFromString("How to create a slug");
+
+  expect(slug.value).toEqual("how-to-create-a-slug");
+});
---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,29 @@
+export class Slug {
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  /**
+   * Receives a string and normalizes it to a slug.
+   *
+   * Example: "An example text" -> "an-example-text"
+   *
+   * @param text {string}
+   */
+  static createFromString(text: string): Slug {
+    const slug = text
+      .normalize("NFKD")
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, "-")
+      .replace(/[^\w-]+/g, "")
+      .replace(/_/g, "-")
+      .replace(/--+/g, "-")
+      .replace(/^-/, "")
+      .replace(/-$/, "");
+
+    return new Slug(slug);
+  }
+}
